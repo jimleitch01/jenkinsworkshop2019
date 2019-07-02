99070bc479a44859eaa08bfe41f449479960457d
Jim Leitch <jim.leitch@oelan.nl>
2019-07-02 20:48:26 +0200

    still having TeeFing troubles 2

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -14,23 +14,22 @@ resource "azurerm_lb" "lb" {
 
   frontend_ip_configuration {
     name                 = "LoadBalancerFrontEnd"
-    public_ip_address_id = "azurerm_public_ip.lbpip.id"
+    public_ip_address_id = "${azurerm_public_ip.lbpip.id}"
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "backend_pool" {
   resource_group_name = "azurerm_resource_group.rg.name"
-  loadbalancer_id     = "azurerm_lb.lb.id"
+  loadbalancer_id     = "${azurerm_lb.lb.id}"
   name                = "BackendPool1"
 }
 
 resource "azurerm_lb_nat_rule" "tcp" {
-  resource_group_name = "azurerm_resource_group.rg.name"
-  loadbalancer_id     = "azurerm_lb.lb.id"
-  name                = "RDP-VM-${count.index}"
-  protocol            = "tcp"
-  frontend_port       = "format(%05d, count.index + 50000)"
-
+  resource_group_name            = "azurerm_resource_group.rg.name"
+  loadbalancer_id                = "${azurerm_lb.lb.id}"
+  name                           = "RDP-VM-${count.index}"
+  protocol                       = "tcp"
+  frontend_port                  = "format(%05d, count.index + 50000)"
   backend_port                   = 3389
   frontend_ip_configuration_name = "LoadBalancerFrontEnd"
   count                          = "${var.number_of_workstations}"
