d2c841b25c45da7584f9eaa06a4bf7c5678be60b
tester <johanvm71@hotmail.com>
2019-07-04 13:49:52 +0000

    test

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -20,7 +20,7 @@ import java.io.Serializable;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.MappedSuperclass; 
 
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects
